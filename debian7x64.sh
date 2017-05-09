de8cbe84b3378dee702ba2d8c5e5a0e32464baa5
danangtrihermansyah <danang13071998@gmail.com>
2017-05-10 02:27:25 +0700

    Update debian7x64.sh

--- debian7x64.sh
+++ debian7x64.sh
@@ -71,14 +71,14 @@ apt-get install boxes
 # squid3
 apt-get update
 apt-get -y install squid3
-wget -O /etc/squid3/squid.conf "https://raw.githubusercontent.com/denysiswanto/autoscript2/master/squid/squid.conf"
+wget -O /etc/squid3/squid.conf "https://raw.githubusercontent.com/danangtrihermansyah/premium/master/squid/squid.conf"
 sed -i "s/ipserver/$myip/g" /etc/squid3/squid.conf
 chmod 0640 /etc/squid3/squid.conf
 
 # text warna
 cd
 rm -rf .bashrc
-wget -O .bashrc https://raw.githubusercontent.com/denysiswanto/autoscript2/master/text%20warna/.bashrc
+wget -O .bashrc https://raw.githubusercontent.com/danangtrihermansyah/premium/master/text%20warna/.bashrc
 
 # text pelangi
 sudo apt-get install ruby -y
@@ -88,23 +88,23 @@ sudo gem install lolcat
 apt-get -y install nginx php5-fpm php5-cli
 rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
-wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/denysiswanto/autoscript2/master/conf/nginx.conf"
+wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/danangtrihermansyah/premium/master/conf/nginx.conf"
 mkdir -p /home/vps/public_html
 echo "<pre>Setup by YusufArdiansyah | telegram : e-Server | Pin BBM : yu-suf</pre>" > /home/vps/public_html/index.php
 echo "<?php phpinfo(); ?>" > /home/vps/public_html/info.php
-wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/denysiswanto/autoscript2/master/conf/vps.conf"
+wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/danangtrihermansyah/premium/master/conf/vps.conf"
 sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php5/fpm/pool.d/www.conf
 
 # install openvpn
 apt-get install openvpn -y
-wget -O /etc/openvpn/openvpn.tar "https://raw.github.com/denysiswanto/autoscript2/master/conf/openvpn-debian.tar"
+wget -O /etc/openvpn/openvpn.tar "https://raw.github.com/danangtrihermansyah/premium/master/conf/openvpn-debian.tar"
 cd /etc/openvpn/
 tar xf openvpn.tar
-wget -O /etc/openvpn/1194.conf "https://raw.github.com/denysiswanto/autoscript2/master/conf/1194.conf"
+wget -O /etc/openvpn/1194.conf "https://raw.github.com/danangtrihermansyah/premium/master/conf/1194.conf"
 service openvpn restart
 sysctl -w net.ipv4.ip_forward=1
 sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
-wget -O /etc/iptables.conf "https://raw.github.com/denysiswanto/autoscript2/master/conf/iptables.conf"
+wget -O /etc/iptables.conf "https://raw.github.com/danangtrihermansyah/premium/master/conf/iptables.conf"
 sed -i '$ i\iptables-restore < /etc/iptables.conf' /etc/rc.local
 
 myip2="s/ipserver/$myip/g";
@@ -115,7 +115,7 @@ service openvpn restart
 
 # configure openvpn client config
 cd /etc/openvpn/
-wget -O /etc/openvpn/1194-client.ovpn "https://raw.github.com/denysiswanto/autoscript2/master/conf/1194-client.conf"
+wget -O /etc/openvpn/1194-client.ovpn "https://raw.github.com/danangtrihermansyah/premium/master/conf/1194-client.conf"
 usermod -s /bin/false mail
 echo "mail:deenie" | chpasswd
 useradd -s /bin/false -M deenie11
@@ -134,7 +134,7 @@ service ssh restart
 # install dropbear
 #apt-get install dropbear
 #apt-get install zlib1g-dev
-#wget "https://raw.githubusercontent.com/denysiswanto/autoscript2/master/dropbear201674/dropbear-2016.74.tar.bz2"
+#wget "https://raw.githubusercontent.com/danangtrihermansyah/premium/master/dropbear201674/dropbear-2016.74.tar.bz2"
 #bzip2 -cd dropbear-2016.74.tar.bz2 | tar xvf -
 #cd dropbear-2016.74
 #./configure
@@ -151,7 +151,7 @@ service ssh restart
 service dropbear restart
 
 # bannerssh
-wget -O bannersshlink.sh "https://raw.githubusercontent.com/denysiswanto/autoscript2/master/menu/bannersshlink.sh"
+wget -O bannersshlink.sh "https://raw.githubusercontent.com/danangtrihermansyah/premium/master/menu/bannersshlink.sh"
 chmod 700 bannersshlink.sh
 ./bannersshlink.sh
 rm bannersshlink.sh
@@ -200,28 +200,28 @@ echo "40 * * * * root ./clearcache.sh" > /etc/cron.d/clearcache5
 echo "50 * * * * root ./clearcache.sh" > /etc/cron.d/clearcache6
 
 # auto kill dropbear
-#wget "https://raw.githubusercontent.com/denysiswanto/autoscript2/master/menu/userlimit.sh"
+#wget "https://raw.githubusercontent.com/danangtrihermansyah/premium/master/menu/userlimit.sh"
 #mv ./userlimit /usr/bin/userlimit.sh
 #chmod +x /usr/bin/userlimit.sh
 #echo " /etc/security/limits.conf" > /etc/security/limits.conf
 
 # auto kill openssh
-#wget "https://raw.githubusercontent.com/denysiswanto/autoscript2/master/menu/userlimitssh.sh"
+#wget "https://raw.githubusercontent.com/danangtrihermansyah/premium/master/menu/userlimitssh.sh"
 #mv ./userlimitssh.sh /usr/bin/userlimitssh.sh
 #chmod +x /usr/bin/userlimitssh.sh
 
 # cranjob
 #sudo apt-get install cron
-#wget https://raw.githubusercontent.com/denysiswanto/autoscript2/master/clearcache/crontab
+#wget https://raw.githubusercontent.com/danangtrihermansyah/premium/master/clearcache/crontab
 #mv crontab /etc/
 #chmod 644 /etc/crontab
 
 # tool 
 cd
-wget -O userlimit.sh "https://raw.githubusercontent.com/denysiswanto/autoscript2/master/menu/userlimit.sh"
-wget -O userexpired.sh "https://raw.githubusercontent.com/denysiswanto/autoscript2/master/menu/userexpired.sh"
-#wget -O autokill.sh "https://raw.githubusercontent.com/denysiswanto/autoscript2/master/menu/autokill.sh"
-wget -O userlimitssh.sh "https://raw.githubusercontent.com/denysiswanto/autoscript2/master/menu/userlimitssh.sh"
+wget -O userlimit.sh "https://raw.githubusercontent.com/danangtrihermansyah/premium/master/menu/userlimit.sh"
+wget -O userexpired.sh "https://raw.githubusercontent.com/danangtrihermansyah/premium/master/menu/userexpired.sh"
+#wget -O autokill.sh "https://raw.githubusercontent.com/danangtrihermansyah/premium/master/menu/autokill.sh"
+wget -O userlimitssh.sh "https://raw.githubusercontent.com/danangtrihermansyah/premium/master/menu/userlimitssh.sh"
 echo "@reboot root /root/userexpired.sh" >> /etc/cron.d/userexpired
 #echo "@reboot root /root/userlimit.sh" > /etc/cron.d/userlimit
 #echo "@reboot root /root/userlimitssh.sh" > /etc/cron.d/userlimitssh
@@ -234,19 +234,19 @@ chmod +x userlimitssh.sh
 
 # clear cache
 service cron stop
-wget -O clearcache.sh "https://raw.githubusercontent.com/denysiswanto/autoscript2/master/clearcache/clearcache.sh"
+wget -O clearcache.sh "https://raw.githubusercontent.com/danangtrihermansyah/premium/master/clearcache/clearcache.sh"
 mv ./clearcache.sh /root/clearcache.sh
 #echo "@reboot root /root/clearcache.sh" > /etc/cron.d/clearcache
 chmod 755 /root/clearcache.sh
 
 # clear cache squid
-wget -O clearcachesquid.sh "https://raw.githubusercontent.com/denysiswanto/autoscript2/master/menu/clearcachesquid.sh"
+wget -O clearcachesquid.sh "https://raw.githubusercontent.com/danangtrihermansyah/premium/master/menu/clearcachesquid.sh"
 mv ./clearcachesquid.sh /root/clearcachesquid.sh
 chmod ug+x /root/clearcachesquid.sh
 service cron start
 # userlimit
 #cd
-#wget "https://raw.githubusercontent.com/denysiswanto/autoscript2/master/conf/limits.conf"
+#wget "https://raw.githubusercontent.com/danangtrihermansyah/premium/master/conf/limits.conf"
 #mv limits.conf /etc/security/limits.conf
 #chmod 644 /etc/security/limits.conf
 
@@ -257,23 +257,23 @@ iptables -I INPUT -p tcp --dport 80 -j ACCEPT
 # speedtest
 cd
 apt-get install python
-wget -O speedtest.py "https://raw.githubusercontent.com/denysiswanto/autoscript2/master/menu/speedtest.py"
+wget -O speedtest.py "https://raw.githubusercontent.com/danangtrihermansyah/premium/master/menu/speedtest.py"
 chmod +x speedtest.py
 
 # Install Menu
 cd
-wget "https://raw.githubusercontent.com/denysiswanto/autoscript2/master/menu/menu"
+wget "https://raw.githubusercontent.com/danangtrihermansyah/premium/master/menu/menu"
 mv ./menu /usr/local/bin/menu
 chmod +x /usr/local/bin/menu
 
 # install bannermenu
-wget -O bannermenu "https://raw.githubusercontent.com/denysiswanto/autoscript2/master/menu/bannermenu"
+wget -O bannermenu "https://raw.githubusercontent.com/danangtrihermansyah/premium/master/menu/bannermenu"
 mv ./bannermenu /root/bannermenu
 chmod +x /root/bannermenu
 
 # update script vps
 cd
-wget -O /usr/local/bin/menu-update-script-vps.sh "https://raw.githubusercontent.com/denysiswanto/autoscript2/master/menu-update-script-vps.sh"
+wget -O /usr/local/bin/menu-update-script-vps.sh "https://raw.githubusercontent.com/danangtrihermansyah/premium/master/menu-update-script-vps.sh"
 mv ./menu-update-script-vps.sh /usr/local/bin/menu-update-script-vps.sh
 chmod +x /usr/local/bin/menu-update-script-vps.sh
 
@@ -285,7 +285,7 @@ apt-get -y install build-essential
 aptitude -y install build-essential
 #shc file
 cd
-wget "https://raw.githubusercontent.com/denysiswanto/autoscript2/master/shc-3.8.7.tgz"
+wget "https://raw.githubusercontent.com/danangtrihermansyah/premium/master/shc-3.8.7.tgz"
 tar xvfz shc-3.8.7.tgz
 #cd shc-3.8.7
 #make
@@ -322,7 +322,7 @@ mkswap /swapfile
 # jalan swapfile
 swapon /swapfile
 #auto star saat reboot
-wget https://raw.githubusercontent.com/denysiswanto/autoscript2/master/ram/fstab
+wget https://raw.githubusercontent.com/danangtrihermansyah/premium/master/ram/fstab
 mv ./fstab /etc/fstab
 chmod 644 /etc/fstab
 sysctl vm.swappiness=10
