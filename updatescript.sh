9e001b60c49cd0839fce2a90ab6bba374b23120a
danangtrihermansyah <danang13071998@gmail.com>
2017-05-12 02:22:51 +0700

    Update updatescript.sh

--- updatescript.sh
+++ updatescript.sh
@@ -1,5 +1,5 @@
 	#!/bin/bash
-echo "Maaf.. Saat ini belum tersedia update script bos......!!!" | boxes -d boy
+echo "Maaf.. Saat ini belum tersedia update script bos......!!!" 
 #echo "Update webmin tersedia bos, tp versi webmin 1.820_all.deb..gk apa2 ya boss.." | lolcat
 sleep 4
 cd
